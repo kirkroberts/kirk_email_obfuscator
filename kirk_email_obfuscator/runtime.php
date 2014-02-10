44bb4935cc4a30d9f3bff80e8b2be94c52b9c150
Kirk Roberts <kirk@kirkrobertsdesign.com>
2014-02-10 15:25:41 -0500

    don't mess with code in the <head>

--- kirk_email_obfuscator/runtime.php
+++ kirk_email_obfuscator/runtime.php
@@ -2,10 +2,22 @@
 
 /*
 
-Version: 0.1
+Version: 0.2
 Author: Kirk Roberts
 
-USAGE
+USAGE: just add the runtime.php to /config/apps.php
+That's it.
+
+
+## Change Log
+
+Version 0.2
+
+- now only replaces emails after the closing </head> tag
+
+Version 0.1
+
+- initial version
 
 */
 
@@ -15,23 +27,30 @@ USAGE
 function kirk_email_obfuscator_callback($buffer)
 {
 	
+	$delimiter = '</head>';
+	$markupParts = explode($delimiter, $buffer);
+	$str = $markupParts[1];
+
 	// find mailto: links
 	$pattern = "|(<a .*mailto:.+</a>)|";
-	preg_match_all($pattern, $buffer, $matches);
+	preg_match_all($pattern, $str, $matches);
 	foreach ($matches[0] as $match) {
 		$replacement = kirk_email_obfuscator_obfuscate_text($match);
-		$buffer = str_replace($match, $replacement, $buffer);
+		$str = str_replace($match, $replacement, $str);
 	}
 	
 	// find bare email addresses
 	$pattern = "/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]+\b(?!([^<]+)?>)/i";
-	preg_match_all($pattern, $buffer, $matches);
+	preg_match_all($pattern, $str, $matches);
 	foreach ($matches[0] as $match) {
 		$replacement = kirk_email_obfuscator_obfuscate_text($match);
-		$buffer = str_replace($match, $replacement, $buffer);
+		$str = str_replace($match, $replacement, $str);
 	}
 	
-	return $buffer;
+	$markupParts[1] = $str;
+
+	return implode($delimiter, $markupParts);
+
 }
 
 function kirk_email_obfuscator_obfuscate_text($text) {
